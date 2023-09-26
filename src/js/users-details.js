d1bc83c8a890096f6d1b4d166b48a56cd4969d57
adreyhann <meliboadrian@gmail.com>
2023-09-26 19:45:09 +0800

    initial commit

---
+++ src/js/users-details.js
@@ -0,0 +1,20 @@
+document.getElementById("load-content-button").addEventListener("click", function() {
+    // URL of the content you want to load
+    const contentUrl = "user-loc.html";
+
+    // Fetch the content using the Fetch API
+    fetch(contentUrl)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error("Network response was not ok");
+            }
+            return response.text();
+        })
+        .then(data => {
+            // Update the content container with the loaded data
+            document.getElementById("content-container").innerHTML = data;
+        })
+        .catch(error => {
+            console.error("Error fetching content:", error);
+        });
+});
