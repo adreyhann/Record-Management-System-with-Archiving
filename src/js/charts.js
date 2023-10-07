4ffc787642c7a47185e590fbd6fadfd7fd3f18a8
adreyhann <meliboadrian@gmail.com>
2023-10-07 20:59:36 +0800

    a

--- src/js/charts.js
+++ src/js/charts.js
@@ -9,16 +9,23 @@ document.addEventListener('DOMContentLoaded', function () {
 				{
 					label: 'Students',
 					data: [20, 30, 40, 20, 30, 20, 10],
+					fill: true,
 					backgroundColor: [
 						'rgba(54, 162, 235, 0.2)',
 						'rgba(255, 99, 132, 0.2)',
+                        'rgba(255, 205, 86, 0.2)',
+						'rgba(153, 102, 255, 0.2)',
                         'rgba(54, 162, 235, 0.2)',
-						'rgba(255, 99, 132, 0.2)',
-                        'rgba(54, 162, 235, 0.2)',
-						'rgba(255, 99, 132, 0.2)',
-                        'rgba(54, 162, 235, 0.2)',
+						'rgba(153, 102, 255, 0.2)',
+                        'rgba(201, 203, 207, 0.2)',
 					],
-					borderColor: ['rgba(54, 162, 235, 1)', 'rgba(255, 99, 132, 1)'],
+					borderColor: [
+						'rgba(54, 162, 235, 1)', 
+						'rgba(255, 99, 132, 1)', 
+						'rgb(255, 205, 86)',
+						'rgb(153, 102, 255)',
+						'rgb(201, 203, 207)'
+					],
 					borderWidth: 1,
 				},
 			],
@@ -30,3 +37,4 @@ document.addEventListener('DOMContentLoaded', function () {
         },
 	});
 });
+
