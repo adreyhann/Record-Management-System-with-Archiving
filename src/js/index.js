bf7b81ab79db945047b788c29b9778209b237447
adreyhann <meliboadrian@gmail.com>
2023-10-13 14:19:00 +0800

    initial commit

--- src/js/index.js
+++ src/js/index.js
@@ -1,18 +1,25 @@
-let correctUsername = "admin";
+let correctUsername = "admin@gmail.com";
 let correctPassword = "qwerty";
+let correctUserUsername = "user@gmail.com";
+let correctUserPassword = "user123";
 
-function login() {
+function login(event) {
 
     event.preventDefault(); 
 
+    let selectedOption = document.getElementById("userType").value;
     let enteredUsername = document.getElementById("username").value;
     let enteredPassword = document.getElementById("password").value;
+    let alertElement = document.getElementById("alert-danger");
 
-    if (enteredUsername === correctUsername && enteredPassword === correctPassword) {
-        window.location.href = "index.html";
+    if (selectedOption === "admin" && enteredUsername === correctUsername && enteredPassword === correctPassword) {
+        window.location.href = "admin-ui/index.html";
+    } else if (selectedOption === "Class Advisor" && enteredUsername === correctUserUsername && enteredPassword === correctUserPassword) {
+        window.location.href = "users-ui/class-advisor-dashboard.html";
     } else {
-        window.alert("Invalid input");
+        alertElement.style.display = "block";
     }
 }
+
 
 document.getElementById("button").addEventListener("click", login);
