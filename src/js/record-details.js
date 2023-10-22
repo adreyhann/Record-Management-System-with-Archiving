1582aff05c7ebc20e5de25cfa4740f0b07d19b05
adreyhann <meliboadrian@gmail.com>
2023-10-22 18:09:35 +0800

    a

--- src/js/record-details.js
+++ src/js/record-details.js
@@ -12,31 +12,7 @@ document.getElementById("settings").addEventListener("click", function() {
             return response.text();
         })
         .then(data => {
-            // Update the content container with the loaded data
-            document.getElementById("content-container").innerHTML = data;
-
-        })
-        .catch(error => {
-            console.error("Error fetching content:", error);
-        });
-});
-
-
-document.getElementById("contacts").addEventListener("click", function() {
-    // URL of the content you want to load
-    const contentUrl = "stud-contact.html";
-
-
-    // Fetch the content using the Fetch API
-    fetch(contentUrl)
-        .then(response => {
-            if (!response.ok) {
-                throw new Error("Network response was not ok");
-            }
-            return response.text();
-        })
-        .then(data => {
-            // Update the content container with the loaded data
+            
             document.getElementById("content-container").innerHTML = data;
 
         })
@@ -46,11 +22,10 @@ document.getElementById("contacts").addEventListener("click", function() {
 });
 
 document.getElementById("location").addEventListener("click", function() {
-    // URL of the content you want to load
+    
     const contentUrl = "stud-loc.html";
 
 
-    // Fetch the content using the Fetch API
     fetch(contentUrl)
         .then(response => {
             if (!response.ok) {
@@ -59,7 +34,7 @@ document.getElementById("location").addEventListener("click", function() {
             return response.text();
         })
         .then(data => {
-            // Update the content container with the loaded data
+            
             document.getElementById("content-container").innerHTML = data;
 
         })
--- src/js/users-details.js
+++ src/js/users-details.js
@@ -20,37 +20,6 @@ document.getElementById("settings").addEventListener("click", function() {
         });
 });
 
-document.getElementById("places-lives").addEventListener("click", function() {
-    // URL of the content you want to load
-    const contentUrl = "places-lives.html";
-
-
-    // Fetch the content using the Fetch API
-    fetch(contentUrl)
-        .then(response => {
-            if (!response.ok) {
-                throw new Error("Network response was not ok");
-            }
-            return response.text();
-        })
-        .then(data => {
-            // Update the content container with the loaded data
-            document.getElementById("content-container").innerHTML = data;
-
-            // // Add the 'active' class to the clicked button
-            // document.getElementById("places-lives").classList.add("active");
-
-            // // Remove the 'active' class from other items
-            // const items = document.querySelectorAll(".item");
-            // items.forEach(item => {
-            //     item.classList.remove("active");
-            // });
-        })
-        .catch(error => {
-            console.error("Error fetching content:", error);
-        });
-});
-
 document.getElementById("contact").addEventListener("click", function() {
     // URL of the content you want to load
     const contentUrl = "contact.html";
